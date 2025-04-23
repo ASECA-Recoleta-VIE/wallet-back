e47c1baba166991f3560e0da1b9b331d59fd65b2
LautiGR_116 <lreinoso@mail.austral.edu.ar>
2025-04-23 10:58:33 -0300

    chore: initialize Spring Boot backend project with Kotlin and Gradle

---
+++ walletapi/src/main/kotlin/com/wallet/walletapi/WalletapiApplication.kt
@@ -0,0 +1,11 @@
+package com.wallet.walletapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WalletapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<WalletapiApplication>(*args)
+}
