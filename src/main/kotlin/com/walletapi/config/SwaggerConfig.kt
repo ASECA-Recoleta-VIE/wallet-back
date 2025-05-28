64edc2f636a1b176c822be8088521c14b884c8b2
sonpipe <96549097+sonpipe0@users.noreply.github.com>
2025-05-28 18:53:22 -0300

    Dev (#11)
    * black-box tests made
    
    * fix swagger

--- src/main/kotlin/com/walletapi/config/SwaggerConfig.kt
+++ src/main/kotlin/com/walletapi/config/SwaggerConfig.kt
@@ -1,31 +1,16 @@
 package com.walletapi.config
 
-import io.swagger.v3.oas.models.ExternalDocumentation
-import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.info.License
+import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig {
-    
     @Bean
-    fun walletApiDocs(): OpenAPI {
-        return OpenAPI()
-            .info(
-                Info().title("Wallet API")
-                    .description("Digital wallet management API")
-                    .version("v1.0")
-                    .license(
-                        License().name("API License")
-                            .url("https://your-license-url.com")
-                    )
-            )
-            .externalDocs(
-                ExternalDocumentation()
-                    .description("Wallet API Documentation")
-                    .url("https://your-documentation-url.com")
-            )
+    fun publicApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("public")
+            .pathsToMatch("/**")
+            .build();
     }
 }
