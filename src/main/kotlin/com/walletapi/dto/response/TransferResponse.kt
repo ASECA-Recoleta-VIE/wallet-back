78a62b60bf66561bc646ef314fbf20dbedb62d07
sonpipe0 <pipefc2003@gmail.com>
2025-06-03 02:37:51 -0300

    redone walletapi integration tests

--- src/main/kotlin/com/walletapi/dto/response/TransferResponse.kt
+++ src/main/kotlin/com/walletapi/dto/response/TransferResponse.kt
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(description = "Result of a transfer operation")
 data class TransferResponse(
     @field:Schema(description = "Sender's wallet after transfer")
-    val fromWallet: WalletResponse,
+    val fromWallet: WalletResponse = WalletResponse(),
     
     @field:Schema(description = "Recipient's wallet after transfer")
-    val toWallet: WalletResponse
+    val toWallet: WalletResponse = WalletResponse(),
 )
